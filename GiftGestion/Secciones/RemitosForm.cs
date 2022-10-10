8175cbab52b0a52229daf44e2faf9a35cbe03c6e
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-10 18:24:40 -0300

    add generar remito pdf

--- GiftGestion/Secciones/RemitosForm.cs
+++ GiftGestion/Secciones/RemitosForm.cs
@@ -28,11 +28,14 @@ namespace GiftGestion.Secciones
         List<Producto> productosOCCarga = new List<Producto>();
         List<Producto> precargaDetalleRemitos = new List<Producto>();
         Remito remitoSeleccionado = new Remito();
+        List<Producto> detalleRemitoSeleccionado = new List<Producto>();
 
         private string rutaModeloRemito = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/GIFT Gestion/Files/remito.xlsx";
         private string rutaModeloRecorrido = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/GIFT Gestion/Files/recorrido.xlsx";
         private string rutaSalidaRemitos = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/GIFT Gestion/Remitos/";
         private string rutaSalidaRecorrido = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/GIFT Gestion/Recorridos/";
+        private string rutaModeloPDF = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/GIFT Gestion/Files/comprobante.docx";
+        private string rutaSalida = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/GIFT Gestion/Remitos/";
 
         Color redColor = Color.FromArgb(180, 63, 38);
         Color yellowColor = Color.FromArgb(216, 231, 20);
@@ -149,11 +152,12 @@ namespace GiftGestion.Secciones
                 if (dataGridRemitos.Rows[e.RowIndex].Cells[0].Value != null)
                 {
                     dataGridProductos.Rows.Clear();
-
+                    detalleRemitoSeleccionado.Clear();
                     foreach (var producto in precargaDetalleRemitos)
                     {
                         if (producto.foranea.Equals(dataGridRemitos.Rows[e.RowIndex].Cells[0].Value.ToString()))
                         {
+                            detalleRemitoSeleccionado.Add(producto);
                             dataGridProductos.Rows.Add(producto.id, producto.nombre_articulo, producto.descripcion, producto.cantidad, producto.proveedor, producto.estacion, producto.color, producto.talle, producto.grupo, producto.precio_lista, producto.precio_efectivo, producto.costo);
                         }
                     }
@@ -339,13 +343,13 @@ namespace GiftGestion.Secciones
 
         private void buttonExportar_Click(object sender, EventArgs e)
         {
+            CreateWordDocument(rutaModeloPDF, rutaSalida + "REMITO_"+remitoSeleccionado.tipo.Substring(0,1).ToUpper()+ "_" + remitoSeleccionado.observacion + ".pdf");
             /*
             try
             {
-
                 SLDocument sl = new SLDocument(rutaModeloRemito);
 
-                sl.SetCellValue("D6", remitoSeleccionado.id);
+                sl.SetCellValue("proveedor", remitoSeleccionado.id);
                 sl.SetCellValue("D8", remitoSeleccionado.fecha);
                 sl.SetCellValue("D9", remitoSeleccionado.hora);
                 sl.SetCellValue("H8", remitoSeleccionado.tipo);
@@ -380,43 +384,6 @@ namespace GiftGestion.Secciones
             {
                 MessageBox.Show(es.Message);
             }*/
-
-            try
-            {
-                 SLDocument sl = new SLDocument(rutaModeloRemito);
-
-                int j = 2;
-                foreach (var remito in remitosCarga)
-                {
-                    if (remito.tipo.Equals("Traspaso"))
-                    {
-                        foreach (var producto in precargaDetalleRemitos)
-                        {
-                            if (producto.foranea.Equals(remito.id))
-                            {
-                                sl.SetCellValue("A" + j.ToString(), producto.id);
-                                sl.SetCellValue("B" + j.ToString(), producto.nombre_articulo);
-                                sl.SetCellValue("C" + j.ToString(), producto.descripcion);
-                                sl.SetCellValue("D" + j.ToString(), producto.costo);
-                                sl.SetCellValue("E" + j.ToString(), producto.cantidad);
-                                sl.SetCellValue("F" + j.ToString(), producto.proveedor);
-                                sl.SetCellValue("G" + j.ToString(), remito.fecha);
-                                sl.SetCellValue("H" + j.ToString(), remito.destino);
-                                j++;
-                            }
-                            
-                           
-                        }
-                    }
-                }
-
-                sl.SaveAs(rutaSalidaRemitos + "ExportacionRemitoEntrada.xlsx");
-                MessageBox.Show("Se Exporto", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }catch(Exception es)
-            {
-
-            }
-
         }
 
         private void recorridoProducto(string id)
@@ -504,6 +471,129 @@ namespace GiftGestion.Secciones
                 }
             }
         }
+
+        private void CreateWordDocument(object filename, object SaveAs)
+        {
+            Word.Application wordApp = new Word.Application();
+            object missing = Missing.Value;
+            Word.Document myWordDoc = null;
+            try
+            {
+                if (File.Exists((string)filename))
+                {
+                    buttonExportar.Enabled = false;
+                    buttonExportar.Text = "Espere pofavor...";
+                    object readOnly = false;
+                    object isVisible = false;
+                    wordApp.Visible = false;
+                    myWordDoc = wordApp.Documents.Open(ref filename, ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing);
+                    myWordDoc.Activate();
+
+                    this.FindAndReplace(wordApp, "<observacion>", remitoSeleccionado.observacion);
+                    this.FindAndReplace(wordApp, "<hacia>", remitoSeleccionado.destino);
+                    this.FindAndReplace(wordApp, "<nombrecomprobante>", "REMITO");
+                    this.FindAndReplace(wordApp, "<numerocomprobante>", remitoSeleccionado.id);
+                    this.FindAndReplace(wordApp, "<Fecha>", remitoSeleccionado.fecha);
+                    this.FindAndReplace(wordApp, "<tipo>", remitoSeleccionado.tipo.ToUpper());
+
+                    int i = 1;
+                    int total = 0;
+                    int subtotal = 0;
+                    int cant = 0;
+                    foreach (var producto in detalleRemitoSeleccionado)
+                    {
+                        subtotal = Int32.Parse(producto.costo) * Int32.Parse(producto.cantidad);
+                        total += subtotal;
+                        cant += Int32.Parse(producto.cantidad);
+                        this.FindAndReplace(wordApp, "<desc" + i.ToString() + ">", producto.nombre_articulo+ " | "+producto.descripcion+" | "+producto.talle+" | "+producto.color);
+                        this.FindAndReplace(wordApp, "<cant" + i.ToString() + ">", producto.cantidad);
+                        this.FindAndReplace(wordApp, "<costo" + i.ToString() + ">", agregarPuntos(producto.costo));
+                        this.FindAndReplace(wordApp, "<subt" + i.ToString() + ">", agregarPuntos(subtotal.ToString()));
+                        i++;
+                    }
+                    this.FindAndReplace(wordApp, "<monto>", agregarPuntos(total.ToString()));
+                    this.FindAndReplace(wordApp, "<cant>", cant.ToString());
+
+
+                    myWordDoc.SaveAs2(ref SaveAs, WdSaveFormat.wdFormatPDF, ref missing, ref missing, ref missing,
+                           ref missing, ref missing, ref missing,
+                           ref missing, ref missing, ref missing,
+                           ref missing, ref missing, ref missing,
+                           ref missing, ref missing, ref missing);
+                    object saveOption = Word.WdSaveOptions.wdDoNotSaveChanges;
+                    object originalFormat = Word.WdOriginalFormat.wdOriginalDocumentFormat;
+                    object routeDocument = false;
+                    myWordDoc.Close(ref saveOption, ref originalFormat, ref routeDocument);
+                    wordApp.Quit();
+                    MessageBox.Show("Se generó Comprobante | Carpeta: Comprobantes/comprobante.pdf", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    buttonExportar.Enabled = true;
+                    buttonExportar.Text = "Generar PDF";
+                }
+            }
+
+            catch (Exception es)
+            {
+                myWordDoc.SaveAs2(ref SaveAs, WdSaveFormat.wdFormatPDF, ref missing, ref missing, ref missing,
+                        ref missing, ref missing, ref missing,
+                        ref missing, ref missing, ref missing,
+                        ref missing, ref missing, ref missing,
+                        ref missing, ref missing, ref missing);
+
+                object saveOption = Word.WdSaveOptions.wdDoNotSaveChanges;
+                object originalFormat = Word.WdOriginalFormat.wdOriginalDocumentFormat;
+                object routeDocument = false;
+                myWordDoc.Close(ref saveOption, ref originalFormat, ref routeDocument);
+                wordApp.Quit();
+                MessageBox.Show("Ocurrio un error");
+                MessageBox.Show(es.Message);
+            }
+        }
+        private void FindAndReplace(Word.Application wordApp, object ToFindText, object replaceWithText)
+        {
+            object matchCase = true;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundLike = false;
+            object nmatchAllforms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiactitics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+
+            wordApp.Selection.Find.Execute(ref ToFindText,
+                ref matchCase, ref matchWholeWord,
+                ref matchWildCards, ref matchSoundLike,
+                ref nmatchAllforms, ref forward,
+                ref wrap, ref format, ref replaceWithText,
+                ref replace, ref matchKashida,
+                ref matchDiactitics, ref matchAlefHamza,
+                ref matchControl);
+        }
+        private string agregarPuntos(string numero)
+        {
+            string num = "";
+            try
+            {
+                int nroNum = Convert.ToInt32(numero);
+                num = nroNum.ToString("N0");
+            }
+            catch (Exception es)
+            {
+
+            }
+            return num;
+        }
+
     }
 
 
